931996019bf7602a8708b97c583b388edd5ef4a5
cjgmj <cjgmj@hotmail.com>
2020-08-06 15:02:46 +0200

    Columnas y estilos

--- contador/lib/src/pages/home_page.dart
+++ contador/lib/src/pages/home_page.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  final estiloTexto = TextStyle(fontSize: 25);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text('Título'), centerTitle: true),
-        body: Center(child: Text('Hola Mundo')));
+        body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+              Text('Número de taps:', style: estiloTexto),
+              Text('0', style: estiloTexto)
+            ])));
   }
 }
