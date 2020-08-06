5ce1ff8d9996bf26d2de5c2ff3344d9146ecf411
cjgmj <cjgmj@hotmail.com>
2020-08-06 14:52:06 +0200

    Widget - Scaffold

--- contador/lib/src/app.dart
+++ contador/lib/src/app.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
+import 'package:contador/src/pages/home_page.dart';
+
 // Widget
 // Todos los Widgets son clases
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Este Widget permite la configuración global de la aplicación
-    return MaterialApp(home: Center(child: Text('Hola Mundo')));
+    return MaterialApp(home: Center(child: HomePage()));
   }
 }
