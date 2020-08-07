084c6014b8447f038819066c22f40cbdbab50bce
cjgmj <cjgmj@hotmail.com>
2020-08-07 14:58:34 +0200

    ListTiles a partir de lista estática

--- componentes/lib/src/pages/home_temp.dart
+++ componentes/lib/src/pages/home_temp.dart
@@ -1,18 +1,44 @@
 import 'package:flutter/material.dart';
 
 class HomeTempPage extends StatelessWidget {
+  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text('Componente Temp')),
         body: ListView(
-          children: <Widget>[
-            ListTile(title: Text('ListTile Title')),
-            Divider(),
-            ListTile(title: Text('ListTile Title')),
-            Divider(),
-            ListTile(title: Text('ListTile Title')),
-          ],
+          // children: _crearItems(),
+          children: _crearItemsCorto(),
         ));
+  }
+
+  List<Widget> _crearItems() {
+    List<Widget> lista = new List<Widget>();
+
+    for (String opt in opciones) {
+      final tempWidget = ListTile(title: Text(opt));
+
+      lista..add(tempWidget)..add(Divider());
+    }
+
+    return lista;
+  }
+
+  List<Widget> _crearItemsCorto() {
+    return opciones
+        .map((opt) => Column(
+              children: [
+                ListTile(
+                  title: Text(opt),
+                  subtitle: Text('Subtítulo $opt'),
+                  leading: Icon(Icons.account_circle),
+                  trailing: Icon(Icons.keyboard_arrow_right),
+                  onTap: () {},
+                ),
+                Divider()
+              ],
+            ))
+        .toList();
   }
 }
