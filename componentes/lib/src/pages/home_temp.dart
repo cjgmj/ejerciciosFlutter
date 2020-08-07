04dbb26e1139ec6bb83bfaeba3f16fdd9e149073
cjgmj <cjgmj@hotmail.com>
2020-08-07 14:40:57 +0200

    Listview

--- componentes/lib/src/pages/home_temp.dart
+++ componentes/lib/src/pages/home_temp.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 
-class HomePageTemp extends StatelessWidget {
+class HomeTempPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Home Temp'));
+    return Scaffold(
+        appBar: AppBar(title: Text('Componente Temp')),
+        body: ListView(
+          children: <Widget>[
+            ListTile(title: Text('ListTile Title')),
+            Divider(),
+            ListTile(title: Text('ListTile Title')),
+            Divider(),
+            ListTile(title: Text('ListTile Title')),
+          ],
+        ));
   }
 }
