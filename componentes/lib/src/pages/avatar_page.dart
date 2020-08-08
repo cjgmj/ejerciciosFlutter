0cbe12c7b7d2864e714a7514c8535f488fdb76b7
cjgmj <cjgmj@hotmail.com>
2020-08-08 13:07:01 +0200

    Widget - Circle Avatar

--- componentes/lib/src/pages/avatar_page.dart
+++ componentes/lib/src/pages/avatar_page.dart
@@ -4,7 +4,28 @@ class AvatarPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Avatar Page')),
-    );
+        appBar: AppBar(
+          title: Text('Avatar Page'),
+          actions: <Widget>[
+            Container(
+              padding: EdgeInsets.all(5),
+              child: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      'https://lh5.ggpht.com/-O97UXKgJn2g/TmspedhwbjI/AAAAAAAAJcY/qUtlXcduCwA/s72-c/image%25255B3%25255D.png'),
+                  radius: 30),
+            ),
+            Container(
+              margin: EdgeInsets.only(right: 10),
+              child: CircleAvatar(
+                  child: Text('JD'), backgroundColor: Colors.brown),
+            )
+          ],
+        ),
+        body: Center(
+            child: FadeInImage(
+                placeholder: AssetImage('assets/jar-loading.gif'),
+                fadeInDuration: Duration(milliseconds: 200),
+                image: NetworkImage(
+                    'https://photographylife.com/wp-content/uploads/2017/01/What-is-landscape-photography.jpg'))));
   }
 }
