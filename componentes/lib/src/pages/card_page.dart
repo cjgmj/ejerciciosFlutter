d5eed1f176ef41dc12853784fb2b5f75751e7def
cjgmj <cjgmj@hotmail.com>
2020-08-08 12:20:17 +0200

    Image y FadeIn Image

--- componentes/lib/src/pages/card_page.dart
+++ componentes/lib/src/pages/card_page.dart
@@ -5,11 +5,9 @@ class CardPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Cards Page')),
-      body: ListView(
-        padding: EdgeInsets.all(10),
-        // padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        children: <Widget>[_cardTipo1()],
-      ),
+      body: ListView(padding: EdgeInsets.all(10),
+          // padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          children: <Widget>[_cardTipo1(), SizedBox(height: 30), _cardTipo2()]),
     );
   }
 
@@ -30,6 +28,29 @@ class CardPage extends StatelessWidget {
               FlatButton(child: Text('Ok'), onPressed: () {})
             ],
           )
+        ],
+      ),
+    );
+  }
+
+  Widget _cardTipo2() {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          FadeInImage(
+              placeholder: AssetImage('assets/jar-loading.gif'),
+              fadeInDuration: Duration(milliseconds: 200),
+              height: 300,
+              fit: BoxFit.cover,
+              image: NetworkImage(
+                  'https://upload.wikimedia.org/wikipedia/commons/c/c8/Untersberg_Mountain_Salzburg_Austria_Landscape_Photography_%28256594075%29.jpeg')),
+
+          // Image(
+          //     image: NetworkImage(
+          //         'https://upload.wikimedia.org/wikipedia/commons/c/c8/Untersberg_Mountain_Salzburg_Austria_Landscape_Photography_%28256594075%29.jpeg')),
+
+          Container(
+              padding: EdgeInsets.all(10), child: Text('Texto de la imagen'))
         ],
       ),
     );
