8ac2d2befce6cf097eab726a479f51bd17ef378f
cjgmj <cjgmj@hotmail.com>
2020-08-09 13:25:11 +0200

    Cambiar el idioma del Datepicker

--- componentes/lib/main.dart
+++ componentes/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_localizations/flutter_localizations.dart';
+
 import 'package:componentes/src/routes/routes.dart';
 
 import 'package:componentes/src/pages/alert_page.dart';
@@ -12,6 +14,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Componentes App',
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', 'US'),
+        const Locale('es', 'ES'),
+      ],
       // home: HomePage(),
       initialRoute: '/',
       routes: getApplicationRoutes(),
