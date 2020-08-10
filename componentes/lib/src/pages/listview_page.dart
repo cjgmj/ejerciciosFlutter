05f1e0fcbcdd03ef8d403c1874fa27fe0f5e3fde
cjgmj <cjgmj@hotmail.com>
2020-08-10 15:53:42 +0200

    InfiniteScroll con Futures

--- componentes/lib/src/pages/listview_page.dart
+++ componentes/lib/src/pages/listview_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'dart:async';
+
 class ListaPage extends StatefulWidget {
   @override
   _ListaPageState createState() => _ListaPageState();
@@ -10,6 +12,7 @@ class _ListaPageState extends State<ListaPage> {
 
   List<int> _listaNumeros = new List();
   int _ultimoItem = 0;
+  bool _isLoading = false;
 
   @override
   void initState() {
@@ -21,16 +24,24 @@ class _ListaPageState extends State<ListaPage> {
       if (_scrollController.position.pixels ==
           _scrollController.position.maxScrollExtent) {
         print('Max scroll');
-        _agregar10();
+        // _agregar10();
+        _fetchData();
       }
     });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    _scrollController.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Listas')),
-      body: _crearLista(),
+      body: Stack(children: <Widget>[_crearLista(), _crearLoading()]),
     );
   }
 
@@ -44,6 +55,8 @@ class _ListaPageState extends State<ListaPage> {
           return FadeInImage(
               placeholder: AssetImage('assets/jar-loading.gif'),
               fadeInDuration: Duration(milliseconds: 200),
+              height: 300,
+              fit: BoxFit.cover,
               image:
                   NetworkImage('https://picsum.photos/500/300?imagen=$imagen'));
         });
@@ -56,5 +69,39 @@ class _ListaPageState extends State<ListaPage> {
     }
 
     setState(() {});
+  }
+
+  Future _fetchData() async {
+    _isLoading = true;
+
+    setState(() {});
+
+    final duration = new Duration(seconds: 2);
+    return new Timer(duration, _respuestaHttp);
+  }
+
+  void _respuestaHttp() {
+    _isLoading = false;
+
+    _scrollController.animateTo(_scrollController.position.pixels + 100,
+        duration: Duration(milliseconds: 250), curve: Curves.fastOutSlowIn);
+
+    _agregar10();
+  }
+
+  Widget _crearLoading() {
+    if (_isLoading) {
+      return Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[CircularProgressIndicator()]),
+            SizedBox(height: 15)
+          ]);
+    } else {
+      return Container();
+    }
   }
 }
