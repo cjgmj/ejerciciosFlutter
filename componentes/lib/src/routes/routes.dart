214d5b4adb3b7b0b0734ce512f69801fb6946b36
cjgmj <cjgmj@hotmail.com>
2020-08-10 15:24:29 +0200

    Listview Builder

--- componentes/lib/src/routes/routes.dart
+++ componentes/lib/src/routes/routes.dart
@@ -7,6 +7,7 @@ import 'package:componentes/src/pages/card_page.dart';
 import 'package:componentes/src/pages/animated_container.dart';
 import 'package:componentes/src/pages/input_page.dart';
 import 'package:componentes/src/pages/slider_page.dart';
+import 'package:componentes/src/pages/listview_page.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes() {
   return <String, WidgetBuilder>{
@@ -16,6 +17,7 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     'card': (BuildContext context) => CardPage(),
     'animatedContainer': (BuildContext context) => AnimatedContainerPage(),
     'inputs': (BuildContext context) => InputPage(),
-    'slider': (BuildContext context) => SliderPage()
+    'slider': (BuildContext context) => SliderPage(),
+    'list': (BuildContext context) => ListaPage()
   };
 }
