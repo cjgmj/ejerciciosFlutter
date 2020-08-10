c748f759fe23b43cae33010bcc87fcc0704f69a8
cjgmj <cjgmj@hotmail.com>
2020-08-10 16:00:27 +0200

    Pull to refresh

--- componentes/lib/src/pages/listview_page.dart
+++ componentes/lib/src/pages/listview_page.dart
@@ -46,20 +46,34 @@ class _ListaPageState extends State<ListaPage> {
   }
 
   Widget _crearLista() {
-    return ListView.builder(
-        controller: _scrollController,
-        itemCount: _listaNumeros.length,
-        itemBuilder: (BuildContext context, int index) {
-          final imagen = _listaNumeros[index];
+    return RefreshIndicator(
+        child: ListView.builder(
+            controller: _scrollController,
+            itemCount: _listaNumeros.length,
+            itemBuilder: (BuildContext context, int index) {
+              final imagen = _listaNumeros[index];
 
-          return FadeInImage(
-              placeholder: AssetImage('assets/jar-loading.gif'),
-              fadeInDuration: Duration(milliseconds: 200),
-              height: 300,
-              fit: BoxFit.cover,
-              image:
-                  NetworkImage('https://picsum.photos/500/300?imagen=$imagen'));
-        });
+              return FadeInImage(
+                  placeholder: AssetImage('assets/jar-loading.gif'),
+                  fadeInDuration: Duration(milliseconds: 200),
+                  height: 300,
+                  fit: BoxFit.cover,
+                  image: NetworkImage(
+                      'https://picsum.photos/500/300?imagen=$imagen'));
+            }),
+        onRefresh: _obtenerPagina1);
+  }
+
+  Future<void> _obtenerPagina1() async {
+    final duration = new Duration(seconds: 2);
+
+    new Timer(duration, () {
+      _listaNumeros.clear();
+      _ultimoItem++;
+      _agregar10();
+    });
+
+    return Future.delayed(duration);
   }
 
   void _agregar10() {
@@ -71,7 +85,7 @@ class _ListaPageState extends State<ListaPage> {
     setState(() {});
   }
 
-  Future _fetchData() async {
+  Future<void> _fetchData() async {
     _isLoading = true;
 
     setState(() {});
