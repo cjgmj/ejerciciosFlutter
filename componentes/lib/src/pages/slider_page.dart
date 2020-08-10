ca6b7404912cacff2f40b3d8291c6762a62397f5
cjgmj <cjgmj@hotmail.com>
2020-08-10 14:51:36 +0200

    Slider

---
+++ componentes/lib/src/pages/slider_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class SliderPage extends StatefulWidget {
+  @override
+  _SliderPageState createState() => _SliderPageState();
+}
+
+class _SliderPageState extends State<SliderPage> {
+  double _valorSlider = 100;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Sliders')),
+        body: Container(
+            // padding: EdgeInsets.only(top: 50),
+            child: Column(
+          children: <Widget>[_crearSlider(), Expanded(child: _crearImagen())],
+        )));
+  }
+
+  Widget _crearSlider() {
+    return Slider(
+        activeColor: Colors.indigoAccent,
+        label: 'Tamaño de la imagen',
+        // divisions: 20,
+        value: _valorSlider,
+        min: 10,
+        max: 400,
+        onChanged: (valor) {
+          setState(() {
+            // print(valor);
+            _valorSlider = valor;
+          });
+        });
+  }
+
+  Widget _crearImagen() {
+    return FadeInImage(
+        placeholder: AssetImage('assets/jar-loading.gif'),
+        fadeInDuration: Duration(milliseconds: 200),
+        image: NetworkImage(
+            'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ce40bded-2727-4182-ad87-5583e7317c09/d9xd4ff-fcb069e3-8ba6-42d0-8be1-ee76e2f7806e.png/v1/fill/w_1024,h_1154,strp/captain_america__civil_war___iron_man_01_png_by_imangelpeabody_d9xd4ff-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD0xMTU0IiwicGF0aCI6IlwvZlwvY2U0MGJkZWQtMjcyNy00MTgyLWFkODctNTU4M2U3MzE3YzA5XC9kOXhkNGZmLWZjYjA2OWUzLThiYTYtNDJkMC04YmUxLWVlNzZlMmY3ODA2ZS5wbmciLCJ3aWR0aCI6Ijw9MTAyNCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19._l2w3ETI4Gh3D0kdLy42wRWDdNHWpmtFKpWWJuoQszQ'),
+        width: _valorSlider,
+        fit: BoxFit.contain);
+  }
+}
--- componentes/lib/src/routes/routes.dart
+++ componentes/lib/src/routes/routes.dart
@@ -1,4 +1,3 @@
-import 'package:componentes/src/pages/input_page.dart';
 import 'package:flutter/material.dart';
 
 import 'package:componentes/src/pages/home_page.dart';
@@ -6,6 +5,8 @@ import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/card_page.dart';
 import 'package:componentes/src/pages/animated_container.dart';
+import 'package:componentes/src/pages/input_page.dart';
+import 'package:componentes/src/pages/slider_page.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes() {
   return <String, WidgetBuilder>{
@@ -14,6 +15,7 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     'avatar': (BuildContext context) => AvatarPage(),
     'card': (BuildContext context) => CardPage(),
     'animatedContainer': (BuildContext context) => AnimatedContainerPage(),
-    'inputs': (BuildContext context) => InputPage()
+    'inputs': (BuildContext context) => InputPage(),
+    'slider': (BuildContext context) => SliderPage()
   };
 }
