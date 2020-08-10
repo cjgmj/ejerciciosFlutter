1ee37cdb410bdadc2dd6b8b863b6e033fdb197b5
cjgmj <cjgmj@hotmail.com>
2020-08-10 15:38:49 +0200

    InfiniteScroll

--- componentes/lib/src/pages/listview_page.dart
+++ componentes/lib/src/pages/listview_page.dart
@@ -6,7 +6,25 @@ class ListaPage extends StatefulWidget {
 }
 
 class _ListaPageState extends State<ListaPage> {
-  List<int> _listaNumeros = [1, 2, 3, 4, 5];
+  ScrollController _scrollController = new ScrollController();
+
+  List<int> _listaNumeros = new List();
+  int _ultimoItem = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _agregar10();
+
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        print('Max scroll');
+        _agregar10();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +36,7 @@ class _ListaPageState extends State<ListaPage> {
 
   Widget _crearLista() {
     return ListView.builder(
+        controller: _scrollController,
         itemCount: _listaNumeros.length,
         itemBuilder: (BuildContext context, int index) {
           final imagen = _listaNumeros[index];
@@ -28,5 +47,14 @@ class _ListaPageState extends State<ListaPage> {
               image:
                   NetworkImage('https://picsum.photos/500/300?imagen=$imagen'));
         });
+  }
+
+  void _agregar10() {
+    for (var i = 1; i <= 10; i++) {
+      _ultimoItem++;
+      _listaNumeros.add(_ultimoItem);
+    }
+
+    setState(() {});
   }
 }
