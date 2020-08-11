b30b8c703fe8813118b93a5ba14e7714b343e67e
cjgmj <cjgmj@hotmail.com>
2020-08-11 16:05:29 +0200

    Mostrar póster de películas en el Swiper

--- peliculas/lib/src/models/pelicula_model.dart
+++ peliculas/lib/src/models/pelicula_model.dart
@@ -62,4 +62,12 @@ class Pelicula {
     overview = json['overview'];
     releaseDate = json['release_date'];
   }
+
+  getPosterImg() {
+    if (posterPath == null) {
+      return 'https://dpxm37ye02lw5.cloudfront.net/thumbnails/thumb_placeholder.jpg';
+    } else {
+      return 'https://image.tmdb.org/t/p/w500/$posterPath';
+    }
+  }
 }
