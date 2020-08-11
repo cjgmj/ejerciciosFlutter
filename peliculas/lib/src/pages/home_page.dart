016b27b80a58438560de7cf224ffa13742c20229
cjgmj <cjgmj@hotmail.com>
2020-08-11 14:30:00 +0200

    Swiper de tarjetas

--- peliculas/lib/src/pages/home_page.dart
+++ peliculas/lib/src/pages/home_page.dart
@@ -1,8 +1,42 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_swiper/flutter_swiper.dart';
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: SafeArea(child: Text('Hola Mundo')));
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: false,
+          title: Text('Películas en cartelera'),
+          backgroundColor: Colors.indigoAccent,
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.search), onPressed: () {})
+          ],
+        ),
+        body: Container(
+          child: Column(
+            children: <Widget>[_swiperTarjetas()],
+          ),
+        ));
+  }
+
+  Widget _swiperTarjetas() {
+    return Container(
+      padding: EdgeInsets.only(top: 10),
+      width: double.infinity,
+      height: 300,
+      child: Swiper(
+        layout: SwiperLayout.STACK,
+        itemWidth: 200,
+        itemBuilder: (BuildContext context, int index) {
+          return new Image.network("http://via.placeholder.com/350x150",
+              fit: BoxFit.fill);
+        },
+        itemCount: 3,
+        // pagination: new SwiperPagination(),
+        // control: new SwiperControl()
+      ),
+    );
   }
 }
