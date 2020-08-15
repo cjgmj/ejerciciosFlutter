7503f78962f214153d7efc898b62fc25d6653d16
cjgmj <cjgmj@hotmail.com>
2020-08-15 11:22:01 +0200

    Leer un código QR

--- qrreaderapp/lib/src/pages/home_page.dart
+++ qrreaderapp/lib/src/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:barcode_scan/barcode_scan.dart';
+
 import 'package:qrreaderapp/src/pages/direcciones_page.dart';
 import 'package:qrreaderapp/src/pages/mapas_page.dart';
 
@@ -26,8 +28,24 @@ class _HomePageState extends State<HomePage> {
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
         floatingActionButton: FloatingActionButton(
             child: Icon(Icons.filter_center_focus),
-            onPressed: () {},
+            onPressed: _scanQR,
             backgroundColor: Theme.of(context).primaryColor));
+  }
+
+  _scanQR() async {
+    dynamic futureString = '';
+
+    try {
+      futureString = await BarcodeScanner.scan();
+    } catch (e) {
+      futureString = e.toString();
+    }
+
+    print('Future String: ${futureString.rawContent}');
+
+    if (futureString != null) {
+      print('Tenemos información');
+    }
   }
 
   Widget _callPage(int paginaActual) {
