13da740ad407c2ea20c684f4f6fde6afd62da71a
cjgmj <cjgmj@hotmail.com>
2020-08-16 11:58:30 +0200

    Crear registros

--- qrreaderapp/lib/src/providers/db_provider.dart
+++ qrreaderapp/lib/src/providers/db_provider.dart
@@ -4,6 +4,8 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
 
+import 'package:qrreaderapp/src/models/scan_model.dart';
+
 class DBProvider {
   static Database _database;
   static final DBProvider db = DBProvider._();
@@ -27,10 +29,29 @@ class DBProvider {
     return await openDatabase(path, version: 1, onOpen: (db) {},
         onCreate: (Database db, int version) async {
       await db.execute('CREATE TABLE Scans ('
-          'id INTEGER PRIMARY KEY,'
-          'tipo TEXT,'
+          'id INTEGER PRIMARY KEY, '
+          'tipo TEXT, '
           'valor TEXT'
           ')');
     });
+  }
+
+  // Crear registros
+  nuevoScanRaw(ScanModel nuevoScan) async {
+    final db = await database;
+
+    final res = await db.rawInsert("INSERT INTO Scans (id, tipo valor) "
+        "VALUES (${nuevoScan.id}, '${nuevoScan.tipo}', '${nuevoScan.valor}')");
+
+    return res;
+  }
+
+  // Así es como realizaremos las inserciones
+  nuevoScan(ScanModel nuevoScan) async {
+    final db = await database;
+
+    final res = db.insert('Scans', nuevoScan.toJson());
+
+    return res;
   }
 }
