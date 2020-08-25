5b336e6aa9558d6cd31dcc636764301517b165bd
cjgmj <cjgmj@hotmail.com>
2020-08-25 16:05:24 +0200

    Mover a un punto del mapa

--- qrreaderapp/lib/src/pages/mapa_page.dart
+++ qrreaderapp/lib/src/pages/mapa_page.dart
@@ -5,19 +5,24 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:qrreaderapp/src/models/scan_model.dart';
 
 class MapaPage extends StatelessWidget {
+  final map = new MapController();
+
   @override
   Widget build(BuildContext context) {
     final ScanModel scan = ModalRoute.of(context).settings.arguments;
 
     return Scaffold(
         appBar: AppBar(title: Text('Coordenadas QR'), actions: <Widget>[
-          IconButton(icon: Icon(Icons.my_location), onPressed: () {})
+          IconButton(
+              icon: Icon(Icons.my_location),
+              onPressed: () => map.move(scan.getLatLng(), 15))
         ]),
         body: _crearFlutterMap(scan));
   }
 
   Widget _crearFlutterMap(ScanModel scan) {
     return FlutterMap(
+        mapController: map,
         options: MapOptions(center: scan.getLatLng(), zoom: 15),
         layers: [_crearMapa(), _crearMarcadores(scan)]);
   }
@@ -30,12 +35,12 @@ class MapaPage extends StatelessWidget {
         additionalOptions: {
           'accessToken':
               'pk.eyJ1IjoiY2pnbWoiLCJhIjoiY2tlOXltbmU1MmJiYjJ6b2JocWIza2ZxaiJ9.rbpjNhT0JEuzbO0XJ9SPjw',
-          'id': 'mapbox/streets-v11'
+          // 'id': 'mapbox/streets-v11'
           // 'id': 'mapbox/outdoors-v11'
           // 'id': 'mapbox/light-v10'
           // 'id': 'mapbox/dark-v10'
           // 'id': 'mapbox/satellite-v9'
-          // 'id': 'mapbox/satellite-streets-v11'
+          'id': 'mapbox/satellite-streets-v11'
         });
   }
 
