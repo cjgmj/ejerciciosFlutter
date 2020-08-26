435c73ddf8d350472e981c4191b46f2be479b8f2
cjgmj <cjgmj@hotmail.com>
2020-08-26 16:12:27 +0200

    Clase para manejar las preferencias del usuario

--- preferenciasusuarioapp/lib/src/pages/home_page.dart
+++ preferenciasusuarioapp/lib/src/pages/home_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'package:preferenciasusuarioapp/src/share_prefs/preferencias_usuario.dart';
 import 'package:preferenciasusuarioapp/src/widgets/menu_widget.dart';
 
 class HomePage extends StatelessWidget {
   static final String routeName = 'home';
+  final prefs = new PreferenciasUsuario();
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +19,7 @@ class HomePage extends StatelessWidget {
             children: <Widget>[
               Text('Color secundario:'),
               Divider(),
-              Text('Género:'),
+              Text('Género: ${prefs.genero}'),
               Divider(),
               Text('Nombre usuario:'),
               Divider(),
---
+++ preferenciasusuarioapp/lib/src/share_prefs/preferencias_usuario.dart
@@ -0,0 +1,31 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class PreferenciasUsuario {
+  static final PreferenciasUsuario _instancia =
+      new PreferenciasUsuario._internal();
+
+  factory PreferenciasUsuario() {
+    return _instancia;
+  }
+
+  PreferenciasUsuario._internal();
+
+  SharedPreferences _prefs;
+
+  initPrefs() async {
+    this._prefs = await SharedPreferences.getInstance();
+  }
+
+  // Ninguna de estas propiedades se usa
+  // bool _colorSecundario;
+  // int _genero;
+  // String _nombre;
+
+  get genero {
+    return _prefs.getInt('genero') ?? 1;
+  }
+
+  set genero(int value) {
+    this._prefs.setInt('genero', value);
+  }
+}
