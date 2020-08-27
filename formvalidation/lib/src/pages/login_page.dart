a000aab56e181b1bdd5db1223cfa47c32e701979
cjgmj <cjgmj@hotmail.com>
2020-08-27 15:35:55 +0200

    Diseño campos login

--- formvalidation/lib/src/pages/login_page.dart
+++ formvalidation/lib/src/pages/login_page.dart
@@ -4,12 +4,8 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          _crearFondo(context),
-        ],
-      ),
-    );
+        body: Stack(
+            children: <Widget>[_crearFondo(context), _loginForm(context)]));
   }
 
   Widget _crearFondo(BuildContext context) {
@@ -25,12 +21,11 @@ class LoginPage extends StatelessWidget {
         ])));
 
     final circulo = Container(
-      width: 100,
-      height: 100,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(100),
-          color: Color.fromRGBO(255, 255, 255, 0.05)),
-    );
+        width: 100,
+        height: 100,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(100),
+            color: Color.fromRGBO(255, 255, 255, 0.05)));
 
     final texto = Container(
         padding: EdgeInsets.only(top: 80),
@@ -40,16 +35,82 @@ class LoginPage extends StatelessWidget {
           Text('John Doe', style: TextStyle(color: Colors.white, fontSize: 25))
         ]));
 
-    return Stack(
-      children: <Widget>[
-        fondo,
-        Positioned(top: 90, left: 30, child: circulo),
-        Positioned(top: -40, right: -30, child: circulo),
-        Positioned(bottom: -50, right: -10, child: circulo),
-        Positioned(bottom: 120, right: 20, child: circulo),
-        Positioned(bottom: -50, left: -20, child: circulo),
-        texto
-      ],
-    );
+    return Stack(children: <Widget>[
+      fondo,
+      Positioned(top: 90, left: 30, child: circulo),
+      Positioned(top: -40, right: -30, child: circulo),
+      Positioned(bottom: -50, right: -10, child: circulo),
+      Positioned(bottom: 120, right: 20, child: circulo),
+      Positioned(bottom: -50, left: -20, child: circulo),
+      texto
+    ]);
+  }
+
+  Widget _loginForm(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return SingleChildScrollView(
+        child: Column(children: <Widget>[
+      SafeArea(child: Container(height: 180)),
+      Container(
+          width: size.width * 0.85,
+          margin: EdgeInsets.symmetric(vertical: 30),
+          padding: EdgeInsets.symmetric(vertical: 50),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(5),
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 3,
+                    offset: Offset(0, 5),
+                    spreadRadius: 3)
+              ]),
+          child: Column(children: <Widget>[
+            Text('Ingreso', style: TextStyle(fontSize: 20)),
+            SizedBox(height: 60),
+            _crearEmail(),
+            SizedBox(height: 30),
+            _crearPassword(),
+            SizedBox(height: 30),
+            _crearBoton()
+          ])),
+      Text('¿Olvidó la contraseña?'),
+      SizedBox(height: 100)
+    ]));
+  }
+
+  Widget _crearEmail() {
+    return Container(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: TextField(
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+                icon: Icon(Icons.alternate_email, color: Colors.deepPurple),
+                hintText: 'ejemplo@correo.com',
+                labelText: 'Correo electrónico')));
+  }
+
+  Widget _crearPassword() {
+    return Container(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+                icon: Icon(Icons.lock_outline, color: Colors.deepPurple),
+                labelText: 'Contraseña')));
+  }
+
+  Widget _crearBoton() {
+    return RaisedButton(
+        padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+        child: Container(
+          child: Text('Ingresar'),
+        ),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+        elevation: 0,
+        color: Colors.deepPurple,
+        textColor: Colors.white,
+        onPressed: () {});
   }
 }
