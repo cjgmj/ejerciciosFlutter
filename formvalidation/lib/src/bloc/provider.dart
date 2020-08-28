1abc05283c1955695ed6b9e9520cb43ec9177a92
cjgmj <cjgmj@hotmail.com>
2020-08-28 15:36:58 +0200

    Mantener datos en los Streams

--- formvalidation/lib/src/bloc/provider.dart
+++ formvalidation/lib/src/bloc/provider.dart
@@ -5,9 +5,19 @@ import 'package:formvalidation/src/bloc/login_bloc.dart';
 export 'package:formvalidation/src/bloc/login_bloc.dart';
 
 class Provider extends InheritedWidget {
-  final loginBloc = LoginBloc();
+  static Provider _instancia;
 
-  Provider({Key key, Widget child}) : super(key: key, child: child);
+  factory Provider({Key key, Widget child}) {
+    if (_instancia == null) {
+      _instancia = new Provider._internal(key: key, child: child);
+    }
+
+    return _instancia;
+  }
+
+  Provider._internal({Key key, Widget child}) : super(key: key, child: child);
+
+  final loginBloc = LoginBloc();
 
   @override
   bool updateShouldNotify(InheritedWidget oldWidget) => true;
