f4c31019715b1341c5aa3f2e40d99b707e934a35
cjgmj <cjgmj@hotmail.com>
2020-08-28 14:19:25 +0200

    InheritedWidget

--- formvalidation/lib/main.dart
+++ formvalidation/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import 'package:formvalidation/src/bloc/provider.dart';
+
 import 'package:formvalidation/src/pages/home_page.dart';
 import 'package:formvalidation/src/pages/login_page.dart';
 
@@ -15,13 +17,15 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Material App',
-        debugShowCheckedModeBanner: false,
-        initialRoute: 'login',
-        routes: {
-          'login': (BuildContext context) => LoginPage(),
-          'home': (BuildContext context) => HomePage()
-        });
+    return Provider(
+      child: MaterialApp(
+          title: 'Material App',
+          debugShowCheckedModeBanner: false,
+          initialRoute: 'login',
+          routes: {
+            'login': (BuildContext context) => LoginPage(),
+            'home': (BuildContext context) => HomePage()
+          }),
+    );
   }
 }
