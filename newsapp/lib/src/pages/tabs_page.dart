1ddc10e0abd77e1e9b7637f1a5bdf89d438127fb
cjgmj <cjgmj@hotmail.com>
2020-09-03 15:05:35 +0200

    Cambiando PageView con Provider

--- newsapp/lib/src/pages/tabs_page.dart
+++ newsapp/lib/src/pages/tabs_page.dart
@@ -30,7 +30,10 @@ class _Navegacion extends StatelessWidget {
 class _Paginas extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final navegacionModel = Provider.of<_NavegacionModel>(context);
+
     return PageView(
+        controller: navegacionModel.pageController,
         // physics: BouncingScrollPhysics(),
         physics: NeverScrollableScrollPhysics(),
         children: <Widget>[
@@ -42,13 +45,19 @@ class _Paginas extends StatelessWidget {
 
 class _NavegacionModel with ChangeNotifier {
   int _paginaActual = 0;
+  PageController _pageController = new PageController();
 
   int get paginaActual => this._paginaActual;
 
   set paginaActual(int valor) {
     this._paginaActual = valor;
 
+    _pageController.animateToPage(valor,
+        duration: Duration(milliseconds: 250), curve: Curves.easeOut);
+
     // Notifica de los cambios a los Widgets suscritos
     notifyListeners();
   }
+
+  PageController get pageController => this._pageController;
 }
