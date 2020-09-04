3b2b07f77baa6af2d917dee80778d8e5003b73db
cjgmj <cjgmj@hotmail.com>
2020-09-04 15:52:21 +0200

    Obtener noticias por categoría

--- newsapp/lib/src/services/news_service.dart
+++ newsapp/lib/src/services/news_service.dart
@@ -12,6 +12,7 @@ final _APIKEY = 'eec42355683a43da88a2225b35c00e8e';
 
 class NewsService with ChangeNotifier {
   List<Article> headlines = [];
+  String _selectedCategory = 'business';
 
   List<Category> categories = [
     Category(FontAwesomeIcons.building, 'business'),
@@ -23,8 +24,23 @@ class NewsService with ChangeNotifier {
     Category(FontAwesomeIcons.memory, 'technology'),
   ];
 
+  Map<String, List<Article>> categoryArticles = {};
+
   NewsService() {
     this.getTopHeadlines();
+
+    categories.forEach((item) {
+      this.categoryArticles[item.name] = new List();
+    });
+  }
+
+  get selectedCategory => this._selectedCategory;
+
+  set selectedCategory(String valor) {
+    this._selectedCategory = valor;
+    this.getArticledByCategory(valor);
+
+    notifyListeners();
   }
 
   getTopHeadlines() async {
@@ -34,6 +50,23 @@ class NewsService with ChangeNotifier {
     final newsResponse = newsResponseFromJson(resp.body);
 
     this.headlines.addAll(newsResponse.articles);
+
+    notifyListeners();
+  }
+
+  getArticledByCategory(String category) async {
+    if (this.categoryArticles[category].length > 0) {
+      return this.categoryArticles[category];
+    }
+
+    final url =
+        '$_URL_NEWS/top-headlines?apiKey=$_APIKEY&country=gb&category=$category';
+    final resp = await http.get(url);
+
+    final newsResponse = newsResponseFromJson(resp.body);
+
+    this.categoryArticles[category].addAll(newsResponse.articles);
+
     notifyListeners();
   }
 }
