1f1e982ab146dec86a15324fea0553d0fccb5ac5
cjgmj <cjgmj@hotmail.com>
2020-09-04 17:20:24 +0200

    Agregar símbolos personalizados

--- mapbox_mapas/lib/src/views/fullscreenmap.dart
+++ mapbox_mapas/lib/src/views/fullscreenmap.dart
@@ -1,4 +1,9 @@
+import 'dart:typed_data';
+
+import 'package:http/http.dart' as http;
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
 
 class FullScreenMap extends StatefulWidget {
@@ -9,9 +14,33 @@ class FullScreenMap extends StatefulWidget {
 class _FullScreenMapState extends State<FullScreenMap> {
   MapboxMapController mapController;
   final center = LatLng(37.810575, -122.477174);
-  String selectedStyle = 'mapbox://styles/cjgmj/ckeockq4m02331an42jvf4jx8';
+  String selectedStyle = 'mapbox://styles/cjgmj/ckeocmp010kkt19ppjbkf15uh';
+
   final oscuroStyle = 'mapbox://styles/cjgmj/ckeockq4m02331an42jvf4jx8';
   final streetStyle = 'mapbox://styles/cjgmj/ckeocmp010kkt19ppjbkf15uh';
+
+  void _onMapCreate(MapboxMapController controller) {
+    mapController = controller;
+    _onStyleLoaded();
+  }
+
+  void _onStyleLoaded() {
+    addImageFromAsset("assetImage", "assets/custom-icon.png");
+    addImageFromUrl("networkImage", "https://via.placeholder.com/50");
+  }
+
+  /// Adds an asset image to the currently displayed style
+  Future<void> addImageFromAsset(String name, String assetName) async {
+    final ByteData bytes = await rootBundle.load(assetName);
+    final Uint8List list = bytes.buffer.asUint8List();
+    return mapController.addImage(name, list);
+  }
+
+  /// Adds a network image to the currently displayed style
+  Future<void> addImageFromUrl(String name, String url) async {
+    var response = await http.get(url);
+    return mapController.addImage(name, response.bodyBytes);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,9 +60,10 @@ class _FullScreenMapState extends State<FullScreenMap> {
                   geometry: center,
                   textField: 'Montaña creada aquí',
                   textOffset: Offset(0, 2),
-                  textColor: '#16b7c5',
-                  iconImage: 'attraction-15',
-                  iconSize: 3));
+                  // textColor: '#16b7c5',
+                  iconSize: 3,
+                  // iconImage: 'attraction-15',
+                  iconImage: 'assetImage'));
             }),
         SizedBox(height: 5),
         // ZoomIn
@@ -61,6 +91,7 @@ class _FullScreenMapState extends State<FullScreenMap> {
                 selectedStyle = oscuroStyle;
               }
 
+              _onStyleLoaded();
               setState(() {});
             })
       ],
@@ -74,9 +105,5 @@ class _FullScreenMapState extends State<FullScreenMap> {
             'pk.eyJ1IjoiY2pnbWoiLCJhIjoiY2tlOXltbmU1MmJiYjJ6b2JocWIza2ZxaiJ9.rbpjNhT0JEuzbO0XJ9SPjw',
         onMapCreated: _onMapCreate,
         initialCameraPosition: CameraPosition(target: center, zoom: 14));
-  }
-
-  void _onMapCreate(MapboxMapController controller) {
-    mapController = controller;
   }
 }
