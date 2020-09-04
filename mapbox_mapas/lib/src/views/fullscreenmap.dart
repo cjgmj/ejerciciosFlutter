6a236d810b4c5efc6e533abfc11ffe48c3ca9825
cjgmj <cjgmj@hotmail.com>
2020-09-04 16:51:46 +0200

    Cambiar estilo de mapa

--- mapbox_mapas/lib/src/views/fullscreenmap.dart
+++ mapbox_mapas/lib/src/views/fullscreenmap.dart
@@ -8,16 +8,40 @@ class FullScreenMap extends StatefulWidget {
 
 class _FullScreenMapState extends State<FullScreenMap> {
   MapboxMapController mapController;
+  final center = LatLng(37.810575, -122.477174);
+  String selectedStyle = 'mapbox://styles/cjgmj/ckeockq4m02331an42jvf4jx8';
+  final oscuroStyle = 'mapbox://styles/cjgmj/ckeockq4m02331an42jvf4jx8';
+  final streetStyle = 'mapbox://styles/cjgmj/ckeocmp010kkt19ppjbkf15uh';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: MapboxMap(
-            accessToken:
-                'pk.eyJ1IjoiY2pnbWoiLCJhIjoiY2tlOXltbmU1MmJiYjJ6b2JocWIza2ZxaiJ9.rbpjNhT0JEuzbO0XJ9SPjw',
-            onMapCreated: _onMapCreate,
-            initialCameraPosition: const CameraPosition(
-                target: LatLng(37.810575, -122.477174), zoom: 14)));
+        body: crearMapa(),
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            FloatingActionButton(
+                child: Icon(Icons.add_to_home_screen),
+                onPressed: () {
+                  if (selectedStyle == oscuroStyle) {
+                    selectedStyle = streetStyle;
+                  } else {
+                    selectedStyle = oscuroStyle;
+                  }
+
+                  setState(() {});
+                })
+          ],
+        ));
+  }
+
+  MapboxMap crearMapa() {
+    return MapboxMap(
+        styleString: selectedStyle,
+        accessToken:
+            'pk.eyJ1IjoiY2pnbWoiLCJhIjoiY2tlOXltbmU1MmJiYjJ6b2JocWIza2ZxaiJ9.rbpjNhT0JEuzbO0XJ9SPjw',
+        onMapCreated: _onMapCreate,
+        initialCameraPosition: CameraPosition(target: center, zoom: 14));
   }
 
   void _onMapCreate(MapboxMapController controller) {
