aff8a76837507211067c47277f525a0852c6cd24
cjgmj <cjgmj@hotmail.com>
2020-09-04 15:07:41 +0200

    Lista de categorias

--- newsapp/lib/src/services/news_service.dart
+++ newsapp/lib/src/services/news_service.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
 
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'package:newsapp/src/models/category_model.dart';
 import 'package:newsapp/src/models/news_models.dart';
 
 final _URL_NEWS = 'https://newsapi.org/v2';
@@ -9,6 +12,16 @@ final _APIKEY = 'eec42355683a43da88a2225b35c00e8e';
 
 class NewsService with ChangeNotifier {
   List<Article> headlines = [];
+
+  List<Category> categories = [
+    Category(FontAwesomeIcons.building, 'business'),
+    Category(FontAwesomeIcons.tv, 'entertainment'),
+    Category(FontAwesomeIcons.addressCard, 'general'),
+    Category(FontAwesomeIcons.headSideVirus, 'health'),
+    Category(FontAwesomeIcons.vials, 'science'),
+    Category(FontAwesomeIcons.volleyballBall, 'sports'),
+    Category(FontAwesomeIcons.memory, 'technology'),
+  ];
 
   NewsService() {
     this.getTopHeadlines();
