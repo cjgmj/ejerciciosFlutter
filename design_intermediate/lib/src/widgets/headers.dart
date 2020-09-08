59a46f10fe8494e45a66acc86fb1376854a7c783
cjgmj <cjgmj@hotmail.com>
2020-09-08 15:03:22 +0200

    Header pico

--- design_intermediate/lib/src/widgets/headers.dart
+++ design_intermediate/lib/src/widgets/headers.dart
@@ -120,3 +120,49 @@ class _HeaderTriangularPainter extends CustomPainter {
     return true;
   }
 }
+
+class HeaderPico extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: double.infinity,
+        width: double.infinity,
+        // Para añadir colores hexadecimales quitar el # y añadir 0xff
+        child: CustomPaint(
+          painter: _HeaderPicoPainter(),
+        ));
+  }
+}
+
+class _HeaderPicoPainter extends CustomPainter {
+  // El size es el tamaño indicado en el Container
+  @override
+  void paint(Canvas canvas, Size size) {
+    // Debería llamarse paint
+    final lapiz = new Paint();
+
+    // Propiedades
+    lapiz.color = Color(0xff615AAB);
+    // Stroke - pinta bordes
+    // Fill - pinta relleno
+    lapiz.style = PaintingStyle.fill;
+    lapiz.strokeWidth = 2;
+
+    final path = new Path();
+
+    // Dibujar con el path y el lápiz
+    path.lineTo(0, size.height * 0.25);
+    path.lineTo(size.width * 0.5, size.height * 0.30);
+    path.lineTo(size.width, size.height * 0.25);
+    path.lineTo(size.width, 0);
+
+    canvas.drawPath(path, lapiz);
+  }
+
+  // Le ponemos true ya que no es costoso redibujar este diseño
+  // Para diseños más complejos habría que controlar cuando se redibuja
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
