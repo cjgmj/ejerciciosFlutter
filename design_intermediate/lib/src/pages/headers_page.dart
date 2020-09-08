65472940a9c58ab36aeacefebf4f13c9e1109d97
cjgmj <cjgmj@hotmail.com>
2020-09-08 14:19:55 +0200

    Header cuadrado

---
+++ design_intermediate/lib/src/pages/headers_page.dart
@@ -0,0 +1,9 @@
+import 'package:design_intermediate/src/widgets/headers.dart';
+import 'package:flutter/material.dart';
+
+class HeadersPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: HeaderCuadrado());
+  }
+}
