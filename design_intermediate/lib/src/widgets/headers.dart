a1b984ca58755634965d8fd0c4ee56889538a30f
cjgmj <cjgmj@hotmail.com>
2020-09-08 15:49:02 +0200

    Header waves

--- design_intermediate/lib/src/widgets/headers.dart
+++ design_intermediate/lib/src/widgets/headers.dart
@@ -199,10 +199,67 @@ class _HeaderCurvoPainter extends CustomPainter {
     // Dibujar con el path y el lápiz
     path.lineTo(0, size.height * 0.25);
     // Las dos primeras coordenadas indica el punto de inflexión
-    // y las dos últimas indica el punto donde acaba
+    // y las dos últimas indica el punto donde acaba la línea
     path.quadraticBezierTo(
         size.width * 0.5, size.height * 0.4, size.width, size.height * 0.25);
     path.lineTo(size.width, 0);
+
+    canvas.drawPath(path, lapiz);
+  }
+
+  // Le ponemos true ya que no es costoso redibujar este diseño
+  // Para diseños más complejos habría que controlar cuando se redibuja
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class HeaderWave extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: double.infinity,
+        width: double.infinity,
+        // Para añadir colores hexadecimales quitar el # y añadir 0xff
+        child: CustomPaint(
+          painter: _HeaderWavePainter(),
+        ));
+  }
+}
+
+class _HeaderWavePainter extends CustomPainter {
+  // El size es el tamaño indicado en el Container
+  @override
+  void paint(Canvas canvas, Size size) {
+    // Debería llamarse paint
+    final lapiz = new Paint();
+
+    // Propiedades
+    lapiz.color = Color(0xff615AAB);
+    // Stroke - pinta bordes
+    // Fill - pinta relleno
+    lapiz.style = PaintingStyle.fill;
+    lapiz.strokeWidth = 2;
+
+    final path = new Path();
+
+    // Dibujar con el path y el lápiz
+    path.lineTo(0, size.height * 0.25);
+    path.quadraticBezierTo(size.width * 0.25, size.height * 0.3,
+        size.width * 0.5, size.height * 0.25);
+    path.quadraticBezierTo(
+        size.width * 0.75, size.height * 0.2, size.width, size.height * 0.25);
+    path.lineTo(size.width, 0);
+
+    // Hacer footer waves
+    // path.moveTo(0, size.height);
+    // path.lineTo(0, size.height * 0.75);
+    // path.quadraticBezierTo(size.width * 0.25, size.height * 0.8,
+    //     size.width * 0.5, size.height * 0.75);
+    // path.quadraticBezierTo(
+    //     size.width * 0.75, size.height * 0.7, size.width, size.height * 0.75);
+    // path.lineTo(size.width, size.height);
 
     canvas.drawPath(path, lapiz);
   }
