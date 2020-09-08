3c081c8abc7833911a6267b80203d335ac223d10
cjgmj <cjgmj@hotmail.com>
2020-09-08 14:23:00 +0200

    Header bordes redondeados

--- design_intermediate/lib/src/pages/headers_page.dart
+++ design_intermediate/lib/src/pages/headers_page.dart
@@ -4,6 +4,6 @@ import 'package:flutter/material.dart';
 class HeadersPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: HeaderCuadrado());
+    return Scaffold(body: HeaderBordesRedondeados());
   }
 }
--- design_intermediate/lib/src/widgets/headers.dart
+++ design_intermediate/lib/src/widgets/headers.dart
@@ -4,9 +4,22 @@ class HeaderCuadrado extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
-      // Para añadir colores hexadecimales quitar el # y añadir 0xff
-      color: Color(0xff615AAB),
-    );
+        height: 300,
+        // Para añadir colores hexadecimales quitar el # y añadir 0xff
+        color: Color(0xff615AAB));
+  }
+}
+
+class HeaderBordesRedondeados extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 300,
+        // Para añadir colores hexadecimales quitar el # y añadir 0xff
+        decoration: BoxDecoration(
+            color: Color(0xff615AAB),
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(70),
+                bottomRight: Radius.circular(70))));
   }
 }
