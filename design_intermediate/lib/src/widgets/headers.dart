4c859461657a355dcd8b2ef19a1fbef22c8daf2e
cjgmj <cjgmj@hotmail.com>
2020-09-08 14:47:39 +0200

    Header diagonal

--- design_intermediate/lib/src/widgets/headers.dart
+++ design_intermediate/lib/src/widgets/headers.dart
@@ -27,12 +27,49 @@ class HeaderBordesRedondeados extends StatelessWidget {
 class HeaderDiagonal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Transform.rotate(
-      angle: -0.05,
-      child: Container(
-          height: 300,
-          // Para añadir colores hexadecimales quitar el # y añadir 0xff
-          color: Color(0xff615AAB)),
-    );
+    return Container(
+        height: double.infinity,
+        width: double.infinity,
+        // Para añadir colores hexadecimales quitar el # y añadir 0xff
+        child: CustomPaint(
+          painter: _HeaderDiagonalPainter(),
+        ));
+  }
+}
+
+class _HeaderDiagonalPainter extends CustomPainter {
+  // El size es el tamaño indicado en el Container
+  @override
+  void paint(Canvas canvas, Size size) {
+    // Debería llamarse paint
+    final lapiz = new Paint();
+
+    // Propiedades
+    lapiz.color = Color(0xff615AAB);
+    // Stroke - pinta bordes
+    // Fill - pinta relleno
+    lapiz.style = PaintingStyle.fill;
+    lapiz.strokeWidth = 2;
+
+    final path = new Path();
+
+    // Dibujar con el path y el lápiz
+    // moveTo mueve el lápiz sin dibujar
+    // lineTo mueve el lápiz dibujando el recorrido
+    path.moveTo(0, size.height * 0.35);
+    path.lineTo(size.width, size.height * 0.30);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+    // Esta línea no es necesaria para el relleno
+    // path.lineTo(0, size.height * 0.35);
+
+    canvas.drawPath(path, lapiz);
+  }
+
+  // Le ponemos true ya que no es costoso redibujar este diseño
+  // Para diseños más complejos habría que controlar cuando se redibuja
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
   }
 }
