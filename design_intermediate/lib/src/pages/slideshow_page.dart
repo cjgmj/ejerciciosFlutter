5ade84cfc67ca57cebef1ef89e7ae5fd5ec3ade0
cjgmj <cjgmj@hotmail.com>
2020-09-11 16:21:39 +0200

    Cambio de color con animación

--- design_intermediate/lib/src/pages/slideshow_page.dart
+++ design_intermediate/lib/src/pages/slideshow_page.dart
@@ -38,12 +38,15 @@ class _Dot extends StatelessWidget {
   Widget build(BuildContext context) {
     final pageViewIndex = Provider.of<SliderModel>(context).currentPage;
 
-    return Container(
+    return AnimatedContainer(
+        duration: Duration(milliseconds: 200),
         width: 12,
         height: 12,
         margin: EdgeInsets.symmetric(horizontal: 5),
         decoration: BoxDecoration(
-            color: index == pageViewIndex ? Colors.blue : Colors.grey,
+            color: (pageViewIndex >= index - 0.5 && pageViewIndex < index + 0.5)
+                ? Colors.blue
+                : Colors.grey,
             shape: BoxShape.circle));
   }
 }
