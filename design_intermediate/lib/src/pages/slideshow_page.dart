b6a9c25837549fddb88f788e5d7f1c1172a51523
cjgmj <cjgmj@hotmail.com>
2020-09-11 15:02:13 +0200

    Creando un Slideshow

--- design_intermediate/lib/src/pages/slideshow_page.dart
+++ design_intermediate/lib/src/pages/slideshow_page.dart
@@ -5,7 +5,33 @@ import 'package:flutter_svg/flutter_svg.dart';
 class SlideShowPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(child: SvgPicture.asset('assets/svgs/slide-1.svg')));
+    return Scaffold(body: Center(child: _Slides()));
+  }
+}
+
+class _Slides extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: PageView(children: <Widget>[
+      _Slide('assets/svgs/slide-1.svg'),
+      _Slide('assets/svgs/slide-2.svg'),
+      _Slide('assets/svgs/slide-3.svg')
+    ]));
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final String svg;
+
+  _Slide(this.svg);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: double.infinity,
+        height: double.infinity,
+        padding: EdgeInsets.all(30),
+        child: SvgPicture.asset(svg));
   }
 }
