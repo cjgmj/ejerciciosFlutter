cf80f3e44919c199fce69492654985326e51392d
cjgmj <cjgmj@hotmail.com>
2020-09-11 15:11:58 +0200

    Diseño de los puntos indicadores

--- design_intermediate/lib/src/pages/slideshow_page.dart
+++ design_intermediate/lib/src/pages/slideshow_page.dart
@@ -5,7 +5,34 @@ import 'package:flutter_svg/flutter_svg.dart';
 class SlideShowPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: _Slides()));
+    return Scaffold(
+        body: Center(
+            child: Column(
+      children: <Widget>[Expanded(child: _Slides()), _Dots()],
+    )));
+  }
+}
+
+class _Dots extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: double.infinity,
+        height: 70,
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[_Dot(), _Dot(), _Dot()]));
+  }
+}
+
+class _Dot extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: 12,
+        height: 12,
+        margin: EdgeInsets.symmetric(horizontal: 5),
+        decoration: BoxDecoration(color: Colors.grey, shape: BoxShape.circle));
   }
 }
 
