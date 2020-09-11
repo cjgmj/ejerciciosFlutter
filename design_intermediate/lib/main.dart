d22bca87c769c74ec19807f0e887bcdd46277e44
cjgmj <cjgmj@hotmail.com>
2020-09-11 16:30:12 +0200

    Widget Slideshow

--- design_intermediate/lib/main.dart
+++ design_intermediate/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 // import 'package:design_intermediate/src/retos/cuadrado_animado_page.dart';
 // import 'package:design_intermediate/src/labs/circular_progress_page.dart';
 // import 'package:design_intermediate/src/pages/graficas_circulares_page.dart';
+// import 'package:design_intermediate/src/labs/slideshow_page.dart';
 import 'package:design_intermediate/src/pages/slideshow_page.dart';
 
 void main() => runApp(MyApp());
@@ -15,6 +16,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Diseños App',
-        home: SlideShowPage());
+        home: SlideshowPage());
   }
 }
