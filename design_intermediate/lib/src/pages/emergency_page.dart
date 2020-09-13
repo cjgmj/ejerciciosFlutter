b159f3e91368f26e86bb119cb5602e83463a0485
cjgmj <cjgmj@hotmail.com>
2020-09-13 12:45:32 +0200

    Creando los botones

--- design_intermediate/lib/src/pages/emergency_page.dart
+++ design_intermediate/lib/src/pages/emergency_page.dart
@@ -1,17 +1,32 @@
 import 'package:flutter/material.dart';
 
-import 'package:design_intermediate/src/widgets/headers.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'package:design_intermediate/src/widgets/headers.dart';
+import 'package:design_intermediate/src/widgets/boton.dart';
 
 class EmergencyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: IconHeader(
-            icon: FontAwesomeIcons.plus,
-            subtitulo: 'Haz solicitado',
-            titulo: 'Asistencia Médica',
-            color1: Color(0xff526BF6),
-            color2: Color(0xff67ACF2)));
+        body: Center(child: Boton()
+            // PageHeader()
+            ));
+  }
+}
+
+class PageHeader extends StatelessWidget {
+  const PageHeader({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconHeader(
+        icon: FontAwesomeIcons.plus,
+        subtitulo: 'Haz solicitado',
+        titulo: 'Asistencia Médica',
+        color1: Color(0xff526BF6),
+        color2: Color(0xff67ACF2));
   }
 }
