7c6a045c2b443b99ff17a87f1ab9b281152bde13
cjgmj <cjgmj@hotmail.com>
2020-09-14 15:49:41 +0200

    Diseño de la página 1

--- animate_do_app/lib/src/pages/pagina1_page.dart
+++ animate_do_app/lib/src/pages/pagina1_page.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
 
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 class Pagina1Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hola Mundo'),
-      ),
-    );
+        appBar: AppBar(title: Text('Animate_do'), actions: <Widget>[
+          IconButton(icon: FaIcon(FontAwesomeIcons.twitter), onPressed: () {}),
+          IconButton(icon: Icon(Icons.navigate_next), onPressed: () {})
+        ]),
+        floatingActionButton: FloatingActionButton(
+            child: FaIcon(FontAwesomeIcons.play), onPressed: () {}),
+        body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+              Icon(Icons.new_releases, color: Colors.blue, size: 40),
+              Text('Título',
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.w200)),
+              Text('Soy un texto pequeño',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400)),
+              Container(width: 220, height: 2, color: Colors.blue)
+            ])));
   }
 }
