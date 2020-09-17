f13687a73adc00d06ad9c73c17bacd63b40b0b1d
cjgmj <cjgmj@hotmail.com>
2020-09-17 16:12:23 +0200

    Pantalla inicial, customAppBar y exportaciones

--- shoesapp/lib/main.dart
+++ shoesapp/lib/main.dart
@@ -1,23 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'package:shoesapp/src/pages/zapato_page.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ShoesApp',
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
-    );
+        title: 'ShoesApp',
+        debugShowCheckedModeBanner: false,
+        home: ZapatoPage());
   }
 }
