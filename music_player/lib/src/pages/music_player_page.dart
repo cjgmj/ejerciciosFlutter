0448b21e25658bc4501f42a68b309c6bc112328b
cjgmj <cjgmj@hotmail.com>
2020-09-18 17:06:20 +0200

    Imagen del disco

--- music_player/lib/src/pages/music_player_page.dart
+++ music_player/lib/src/pages/music_player_page.dart
@@ -7,8 +7,54 @@ class MusicPlayerPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: <Widget>[CustomAppBar()],
+        children: <Widget>[CustomAppBar(), ImagenDiscoDuracion()],
       ),
+    );
+  }
+}
+
+class ImagenDiscoDuracion extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Row(children: <Widget>[
+      // Disco
+      ImagenDisco()
+
+      // TODO: Barra progreso
+    ]));
+  }
+}
+
+class ImagenDisco extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      width: 250,
+      height: 250,
+      child: ClipRRect(
+          borderRadius: BorderRadius.circular(200),
+          child: Stack(alignment: Alignment.center, children: <Widget>[
+            Image(image: AssetImage('assets/aurora.jpg')),
+            Container(
+                width: 25,
+                height: 25,
+                decoration: BoxDecoration(
+                    color: Colors.black38,
+                    borderRadius: BorderRadius.circular(100))),
+            Container(
+                width: 18,
+                height: 18,
+                decoration: BoxDecoration(
+                    color: Color(0xff1C1C25),
+                    borderRadius: BorderRadius.circular(100)))
+          ])),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(200),
+          gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              colors: <Color>[Color(0xff484750), Color(0xff1E1C24)])),
     );
   }
 }
