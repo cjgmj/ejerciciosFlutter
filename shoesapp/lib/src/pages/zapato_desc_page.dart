7fb055390e92d8bd0f3d4322c03352bc59c0a4c5
cjgmj <cjgmj@hotmail.com>
2020-09-18 14:43:14 +0200

    Precio y comprar ahora

--- shoesapp/lib/src/pages/zapato_desc_page.dart
+++ shoesapp/lib/src/pages/zapato_desc_page.dart
@@ -16,11 +16,36 @@ class ZapatoDescPage extends StatelessWidget {
               ZapatoDescripcion(
                   titulo: 'Nike Air Max 720',
                   descripcion:
-                      "The Nike Air Max 720 goes bigger than ever before with Nike's taller Air unit yet, offering more air underfoot for unimaginable, all-day comfort. Has Air Max gone too far? We hope so.")
+                      "The Nike Air Max 720 goes bigger than ever before with Nike's taller Air unit yet, offering more air underfoot for unimaginable, all-day comfort. Has Air Max gone too far? We hope so."),
+              _PrecioBuyNow()
             ],
           ),
         ))
       ],
     ));
+  }
+}
+
+class _PrecioBuyNow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      child: Container(
+        margin: EdgeInsets.only(top: 20),
+        child: Row(
+          children: <Widget>[
+            Text('\$180.0',
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
+            Spacer(),
+            BotonNaranja(
+              texto: 'Buy now',
+              ancho: 120,
+              alto: 40,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- shoesapp/lib/src/widgets/boton_naranja.dart
+++ shoesapp/lib/src/widgets/boton_naranja.dart
@@ -2,15 +2,17 @@ import 'package:flutter/material.dart';
 
 class BotonNaranja extends StatelessWidget {
   final String texto;
+  final double alto;
+  final double ancho;
 
-  const BotonNaranja({@required this.texto});
+  const BotonNaranja({@required this.texto, this.alto = 50, this.ancho = 150});
 
   @override
   Widget build(BuildContext context) {
     return Container(
         alignment: Alignment.center,
-        width: 150,
-        height: 50,
+        width: this.ancho,
+        height: this.alto,
         decoration: BoxDecoration(
             color: Color(0xffF1A23A), borderRadius: BorderRadius.circular(100)),
         child: Text(texto, style: TextStyle(color: Colors.white)));
