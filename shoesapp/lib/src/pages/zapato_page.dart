ed1e21ae321c0b3eaa9db9b83acb5539dfeba960
cjgmj <cjgmj@hotmail.com>
2020-09-18 14:28:15 +0200

    Botón flotante

--- shoesapp/lib/src/pages/zapato_page.dart
+++ shoesapp/lib/src/pages/zapato_page.dart
@@ -13,17 +13,15 @@ class ZapatoPage extends StatelessWidget {
       SizedBox(height: 20),
       Expanded(
           child: SingleChildScrollView(
-        physics: BouncingScrollPhysics(),
-        child: Column(
-          children: <Widget>[
-            ZapatoSizePreview(),
-            ZapatoDescripcion(
-                titulo: 'Nike Air Max 720',
-                descripcion:
-                    "The Nike Air Max 720 goes bigger than ever before with Nike's taller Air unit yet, offering more air underfoot for unimaginable, all-day comfort. Has Air Max gone too far? We hope so.")
-          ],
-        ),
-      ))
+              physics: BouncingScrollPhysics(),
+              child: Column(children: <Widget>[
+                ZapatoSizePreview(),
+                ZapatoDescripcion(
+                    titulo: 'Nike Air Max 720',
+                    descripcion:
+                        "The Nike Air Max 720 goes bigger than ever before with Nike's taller Air unit yet, offering more air underfoot for unimaginable, all-day comfort. Has Air Max gone too far? We hope so.")
+              ]))),
+      AgregarCarritoBoton(precio: 180)
     ]));
   }
 }
